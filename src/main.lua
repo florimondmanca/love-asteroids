e5d311a9a2887dd307cb3eca11e104a8b6f786ff
Manca Florimond <florimond.manca@student.ecp.fr>
2017-06-14 23:37:58 +0200

    quickfix

--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,6 @@
 -- main.lua
 
 local GameState = require 'lib.gamestate'
-local Timer = require 'core.Timer'
 
 require 'core.SoundManager'
 
@@ -10,5 +9,4 @@ love.math.setRandomSeed(os.time())
 function love.load()
     GameState.registerEvents()
     GameState.switch(require 'scenes.splash')
-    Timer:after(5, function() GameState.switch(require 'scenes.game') end)
 end
--- src/scenes/splash.lua
+++ src/scenes/splash.lua
@@ -4,23 +4,26 @@ local menuScene = require('core.GameScene').new()
 
 menuScene:set('timer', require 'core.Timer')
 
-local spinner = {angle = math.pi/2, span = 0, radius = 50, omega = .5}
+local progressBar = {angle = math.pi/2, span = 0, radius = 30, omega = 1}
 
-function spinner:update(dt)
+function progressBar:update(dt)
     self.angle = self.angle + self.omega * dt
 end
 
-function spinner:draw()
+function progressBar:draw()
     love.graphics.setColor(255, 255, 255, 200)
     love.graphics.setLineWidth(10)
     love.graphics.arc('line', 'open', w/2, h/2, self.radius, self.angle, self.angle + self.span)
 end
 
-menuScene:set('spinner', spinner)
+menuScene:set('progressBar', progressBar)
 
 function menuScene:enter()
     love.graphics.setBackgroundColor(40, 45, 55)
-    self.objects.timer:tween(5, self.objects.spinner, {span = 2*math.pi}, 'in-out-quad')
+    self.objects.timer:tween(5, self.objects.progressBar, {span = 2*math.pi}, 'in-out-quad')
+    self.objects.timer:after(5, function()
+        require('lib.gamestate').switch(require 'scenes.game')
+    end)
 end
 
 
